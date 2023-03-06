a99e8069b82ff3d35ef35de34b48df46157cff22
Dominik Lechl <dominik.lechl@alturos.com>
2023-03-06 14:47:42 +0100

    Implement api client

--- SE2Einzelphase/app/src/main/java/at/aau/edu/lechl/se/singlephase/api/ApiClient.kt
+++ SE2Einzelphase/app/src/main/java/at/aau/edu/lechl/se/singlephase/api/ApiClient.kt
@@ -1,20 +1,37 @@
 package at.aau.edu.lechl.se.singlephase.api
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
+import java.io.BufferedInputStream
+import java.io.ObjectOutputStream
+import java.net.Socket
 
 interface ApiClient {
     suspend fun sendRegistrationNumber(registrationNumber: String): Result<String>
 }
+
+private const val HOST_NAME = "se2-isys.aau.at"
+private const val PORT = 53212
 
 class ApiClientImpl : ApiClient {
 
     override suspend fun sendRegistrationNumber(
         registrationNumber: String
     ): Result<String> = withContext(Dispatchers.IO) {
-        delay(1000L) // fake loading
+        try {
+            Socket(HOST_NAME, PORT).use { socket ->
+                val output = ObjectOutputStream(socket.getOutputStream())
+                val input = BufferedInputStream(socket.getInputStream())
 
-        return@withContext Result.success("1234567890")
+                output.writeUTF(registrationNumber)
+                output.flush()
+
+                val response = input.bufferedReader().readLine()
+
+                Result.success(response)
+            }
+        } catch (error: Throwable) {
+            Result.failure(error)
+        }
     }
 }
